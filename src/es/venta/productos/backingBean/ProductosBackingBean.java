4a150495ee6d6a1084b598a431099c39bf59b4cd
savandy <andicuu@gmail.com>
2017-04-03 14:57:11 +0200

    javadoc + log4j añadidos

--- src/es/venta/productos/backingBean/ProductosBackingBean.java
+++ src/es/venta/productos/backingBean/ProductosBackingBean.java
@@ -2,17 +2,46 @@ package es.venta.productos.backingBean;
 
 import java.util.ArrayList;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import es.venta.productos.bbdd.access.ProductoAccess;
 import es.venta.productos.bbdd.beans.ProductoBean;
 
+/**
+ * CONTROLADOR - Gestion de Catalogo de Productos
+ * 
+ * Aquí encontraremos metodos que toman las decisiones pertinentes al proceso de
+ * muestra del catalogo de productos.
+ * 
+ * @author Valeriu
+ *
+ */
+
 public class ProductosBackingBean {
 
-	ProductoAccess productoAccess = new ProductoAccess();
+	private static final Logger log = LogManager.getLogger(LoginBackingBean.class.getName());
+
+	/**
+	 * Retornamos el catalogo de productos completo, o nulo si no hay productos
+	 * 
+	 * @return Listado de productos | null
+	 */
 
 	public ArrayList<ProductoBean> rellenarProductos() {
 
-		ArrayList<ProductoBean> ret = new ArrayList<ProductoBean>();
-		ret = productoAccess.seleccionarProductos();
+		ProductoAccess productoAccess = null;
+		ArrayList<ProductoBean> ret = null;
+
+		try {
+			productoAccess = new ProductoAccess();
+			ret = productoAccess.seleccionarProductos();
+
+			log.info(ret.size() + " productos encontrados");
+
+		} catch (Exception e) {
+			log.error("Error", e);
+		}
 
 		return ret;
 
